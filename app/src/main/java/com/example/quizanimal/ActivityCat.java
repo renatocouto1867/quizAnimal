79479cb7c516143587e8de123115b130720e82de
renatocouto1867 <renatocouto1867@gmail.com>
2024-03-02 11:15:28 -0300

    02_03_Perguntas

--- app/src/main/java/com/example/quizanimal/ActivityCat.java
+++ app/src/main/java/com/example/quizanimal/ActivityCat.java
@@ -2,14 +2,26 @@ package com.example.quizanimal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ActivityCat extends AppCompatActivity {
-
+    private Button buttonProximo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cat);
+
+        buttonProximo=findViewById(R.id.buttonPerguntas);
+        buttonProximo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ActivityCat.this, ActivityPerguntas.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
---
+++ app/src/main/java/com/example/quizanimal/ActivityPerguntas.java
@@ -0,0 +1,14 @@
+package com.example.quizanimal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityPerguntas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_perguntas);
+    }
+}
